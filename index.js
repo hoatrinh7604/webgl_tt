d303975862791cfb9c49d19fd82e264180e5299b
hoatrinh <hoatv.7604@gmail.com>
2024-05-27 23:55:54 +0700

    **

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
   var loadingBar = document.querySelector("#unity-loading-bar");
   var progressBarFull = document.querySelector("#unity-progress-bar-full");
   var warningBanner = document.querySelector("#unity-warning");
-	arlet("H");
+	alert("H");
   // Shows a temporary message banner/ribbon for a few seconds, or
   // a permanent error message on top of the canvas if type=='error'.
   // If type=='warning', a yellow highlight color is used.
