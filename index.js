e5f8d2ad85ff985ad1101b7b356cd130c7d3b22d
Tuk <hoatv.7604@gmail.com>
2024-06-01 00:44:21 +0700

    d

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
   var loadingBar = document.querySelector("#unity-loading-bar");
   var progressBarFull = document.querySelector("#unity-progress-bar-full");
   var warningBanner = document.querySelector("#unity-warning");
-window.alert("AAO!");
+window.alert("AAO!!!");
   // Shows a temporary message banner/ribbon for a few seconds, or
   // a permanent error message on top of the canvas if type=='error'.
   // If type=='warning', a yellow highlight color is used.
